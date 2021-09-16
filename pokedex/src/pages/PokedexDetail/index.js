f8a6d0037df323932c8b78f8901de61a064a9421
Gabriel <gabriel.devsistema@gmail.com>
2021-09-16 19:15:05 -0300

    finalizando projeto

--- pokedex/src/pages/PokedexDetail/index.js
+++ pokedex/src/pages/PokedexDetail/index.js
@@ -23,7 +23,7 @@ const useStyles = makeStyles({
 
 
 export function PokedexDetail({ poke }) {
-    console.log('props', poke);
+
     const { name } = useParams()
     const [pokemonDetail, setPokemonDetail] = useState()
     const classes = useStyles();
